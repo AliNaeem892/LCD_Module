923af076e657ff5868e005f4f1d04478d872a8af
AliNaeem892 <61121049+AliNaeem892@users.noreply.github.com>
2020-03-31 09:54:01 +0500

    Add files via upload

---
+++ LCD_Module.ino
@@ -0,0 +1,26 @@
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+int output_value;
+
+
+void setup() {
+  
+  lcd.begin(16, 2);
+  lcd.begin(16, 2);  
+  lcd.clear();  
+  lcd.setCursor(0, 0);  
+  lcd.print("     Welcome    "); 
+  
+
+}
+
+void loop() {
+ 
+lcd.setCursor(0, 0);  
+lcd.print("    Moisture"); 
+lcd.setCursor(7, 1);  
+lcd.print(output_value); 
+lcd.print(" %   "); 
+}
